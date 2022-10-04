42a36ef364649009a096b183c8604d43fa4550a9
Kenny <kenji.maddela01@gmail.com>
2022-10-03 22:43:48 -0400

    Establish route of the useState concept

--- src/ui/ConceptContainer.js
+++ src/ui/ConceptContainer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
 import Card from './Card'
 
-function ConceptContainer({ children }) {
-	return <Card className="h-32 w-32">{children}</Card>
+function ConceptContainer({ children, path }) {
+	return (
+		<Link to={path}>
+			<Card className="h-64 w-64 select-none hover:text-white duration-200">
+				{children}
+			</Card>
+		</Link>
+	)
 }
 
 export default ConceptContainer
